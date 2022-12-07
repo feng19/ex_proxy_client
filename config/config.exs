e1b1ff6bc4f913402748e232b22dc23a22b506ae
feng19 <kevin@feng19.com>
2022-12-07 17:58:23 +0800

    feat: update deps

--- config/config.exs
+++ config/config.exs
@@ -1,17 +1,3 @@
 import Config
 
-encrypt_key =
-  System.get_env(
-    "EX_PROXY_ENCRYPT_KEY",
-    "90yT56qlvXmCdrrAnQsdb16HNm7lP6ySqi5tySHIr3o8C+Fr4B8URl5XH0NVssVI"
-  )
-
-config :ex_proxy_client, ExProxyClient.Socks5,
-  port: 9050,
-  url: "ws://127.0.0.1:4000/ws",
-  encrypt: {:once, encrypt_key}
-
-config :ex_proxy_client, ExProxyClient.Http,
-  port: 9060,
-  url: "ws://127.0.0.1:4000/ws",
-  encrypt: {:once, encrypt_key}
+import_config "#{config_env()}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -2,16 +2,16 @@ import Config
 
 encrypt_key =
   System.get_env(
-    "EX_PROXY_ENCRYPT_KEY",
+    "EXPS_ENCRYPT_KEY",
     "90yT56qlvXmCdrrAnQsdb16HNm7lP6ySqi5tySHIr3o8C+Fr4B8URl5XH0NVssVI"
   )
 
 config :ex_proxy_client, ExProxyClient.Socks5,
   port: 9050,
-  url: "ws://exps.fly.dev/ws",
+  url: "ws://127.0.0.1:4000/ws",
   encrypt: {:once, encrypt_key}
 
 config :ex_proxy_client, ExProxyClient.Http,
   port: 9060,
-  url: "ws://exps.fly.dev/ws",
+  url: "ws://127.0.0.1:4000/ws",
   encrypt: {:once, encrypt_key}
